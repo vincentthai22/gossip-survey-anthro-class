1100a49155b2d2dbff95e49283454c7cf565cd35
Vincent Thai <vincentthai22@yahoo.com>
2017-02-26 15:54:35 -0800

    added survey

--- app/src/main/java/com/example/vincent/gossipsurvey/GossipArrayAdapter.java
+++ app/src/main/java/com/example/vincent/gossipsurvey/GossipArrayAdapter.java
@@ -1,12 +1,15 @@
 package com.example.vincent.gossipsurvey;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,9 +19,9 @@ import java.util.List;
  * Created by Vincent on 2/26/2017.
  */
 
-public class GossipArrayAdapter extends ArrayAdapter {
+public class GossipArrayAdapter<T> extends ArrayAdapter {
 
-    private List<String> list;
+    private List<T> list;
     private int resource, headerResource;
 
     public GossipArrayAdapter(Context context, int resource, int headerResource, List<String> list){
@@ -27,17 +30,19 @@ public class GossipArrayAdapter extends ArrayAdapter {
         this.resource = resource;
         this.headerResource = headerResource;
     }
-    public GossipArrayAdapter(Context context, int resource,  List<String> list){
+    public GossipArrayAdapter(Context context, int resource,  List<T> list){
         super(context,resource, list);
         this.list = (ArrayList) list;
         this.resource = resource;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent){
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
         return createViewFromResource(position, convertView, parent, resource);
     }
 
-    public String getItem(int position){
+    @Override
+    public T getItem(int position){
         return list.get(position);
     }
 
@@ -55,9 +60,38 @@ public class GossipArrayAdapter extends ArrayAdapter {
                 text = (TextView) view.findViewById(R.id.nameLabel);
                 img = (ImageView) view.findViewById(R.id.profileImage);
                 Log.d("setText", "" + getItem(position));
-                text.setText(getItem(position));
+                text.setText( (String) getItem(position));
                 return view;
             case R.layout.list_survey_item:
+
+                List<String> questionAndOptions = (ArrayList) getItem(position); //index 0 = question, > 0 = options
+                TextView questionTextView = (TextView) view.findViewById(R.id.questionTextView);
+                LinearLayout buttonBar = (LinearLayout) view.findViewById(R.id.buttonBarLayout);
+                buttonBar.setDividerPadding(10);
+                int width = 0;
+
+                buttonBar.removeAllViews();
+                int i = 0;
+                if(questionAndOptions.size()>1)
+                    for (Object questionOrOption : questionAndOptions) {
+                        if (i == 0)
+                            questionTextView.setText((String) questionOrOption);
+                        else {
+                            Button newOptionButton = new Button(this.getContext());
+                            newOptionButton.setBackgroundColor(Color.GRAY);
+                            newOptionButton.setTextColor(Color.BLACK);
+                            Log.d("questionoroption", (String) questionOrOption);
+                            newOptionButton.setText((String) questionOrOption);
+                            width += newOptionButton.getWidth();
+                            Log.d("width", width+"");
+                            buttonBar.addView(newOptionButton);
+
+                        }
+                        i++;
+                    }
+                else
+                    questionTextView.setText("Survey");
+
                 return view;
 
 
--- app/src/main/java/com/example/vincent/gossipsurvey/NewSurveyActivity.java
+++ app/src/main/java/com/example/vincent/gossipsurvey/NewSurveyActivity.java
@@ -1,12 +1,26 @@
 package com.example.vincent.gossipsurvey;
 
 import android.app.Fragment;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Vincent on 2/26/2017.
@@ -48,7 +62,56 @@ public class NewSurveyActivity extends AppCompatActivity {
 
     public void initialize(){
 
+        ListView surveyList = (ListView)  findViewById(R.id.newSurveyListView);
+        List< List<String> > questionsAndOptionsList = new ArrayList<>();
+        buildList(questionsAndOptionsList);
+        LinearLayout buttonBar = (LinearLayout) findViewById(R.id.buttonBarLayout);
+        TextView questionTextView = (TextView) findViewById(R.id.questionTextView);
+        surveyList.setAdapter(new GossipArrayAdapter<List<String>>(this.getApplicationContext(), R.layout.list_survey_item, questionsAndOptionsList));
 
+    }
+
+    public void buildList(List< List<String> > questionsAndOptionList){
+        int index;
+        AssetManager assetManager = getAssets();
+        try{
+            InputStream questionsAndOptionListStream = assetManager.open("surveyquestions.txt");
+            BufferedReader in = new BufferedReader(new InputStreamReader(questionsAndOptionListStream));
+            String line, entireText ="";
+            while((line = in.readLine()) != null){
+               entireText +=line;
+            }
+            Log.d("entiretext", entireText);
+
+            for(String questionsUnsplit : entireText.split("\\*") ) {
+                List temp = new ArrayList();
+                int counter = 0;
+
+                for(String questions : questionsUnsplit.split("=") ) {
+                    if(counter==0) {
+                        Log.d("questions", questions);
+                        temp.add(questions.trim());
+                    }
+                    else {
+                        for (String options : questions.split("/") ) {
+                            Log.d("options", options);
+                            temp.add(options.trim());
+
+                        }
+                    }
+                    counter++;
+                }
+                questionsAndOptionList.add(temp);
+            }
+
+            for(List l : questionsAndOptionList)
+                for(Object s : l)
+                    Log.d("after all parsing", (String)s);
+
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
     }
 
